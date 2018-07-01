51de4f6627accddd7b0853c6aed74963d4511fe0
Brando Mora <morabrandoi@gmail.com>
2018-07-01 02:18:26 -0400

    put net in function set up for loops for variation started writing results to file

--- NetworkTester.py
+++ NetworkTester.py
@@ -1,4 +1,3 @@
-#mnistTut
 
 import keras
 
@@ -6,20 +5,20 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 
-
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
 
-batch_size = 512
+
 num_classes = 10
-epochs = 100
+batch_size = 128
+epochs = 20
 
 # the data shuffled and split between train and test sets
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000,784)
+x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 
 x_train = x_train.astype("float32")
@@ -31,45 +30,60 @@ x_test /= 255
 print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
-#conver class vectors to binary class matrices
+# conver class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
-#defining model
-model = Sequential()
-model.add(Dense(400, activation="relu", input_shape=(784,)))
-model.add(Dropout(0,2))
+def model_create_and_run(number_of_hidden_layers, nodes_per_hidden_layer):
 
-model.add(Dense(200, activation="relu"))
-model.add(Dropout(0,2))
+    # defining model
+    model = Sequential()
 
-model.add(Dense(100, activation="relu"))
-model.add(Dropout(0,2))
+    model.add(Dense(400, activation="relu", input_shape=(784,)))
+    model.add(Dropout(0, 2))
 
-model.add(Dense(50, activation="relu"))
-model.add(Dropout(0,2))
+    for _ in range(number_of_hidden_layers):
 
-model.add(Dense(25, activation="relu"))
-model.add(Dropout(0,2))
+        model.add(Dense(nodes_per_hidden_layer, activation="relu"))
+        model.add(Dropout(0, 2))
 
-model.add(Dense(num_classes, activation="softmax"))
+    model.add(Dense(num_classes, activation="softmax"))
 
-model.summary()
+    model.summary()
+
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=RMSprop(),
+                  metrics=["accuracy"])
+
+    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss',
+                                               min_delta=0.001,
+                                               patience=0,
+                                               verbose=0,
+                                               mode='auto',
+                                               baseline=None)
+
+    history = model.fit(x_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        callbacks=[early_stop],
+                        verbose=1,
+                        validation_data=(x_test, y_test))
+
+    score = model.evaluate(x_test, y_test, verbose=1)
+    print("Test loss: ", score[0])
+    print("test accuracy: ", score[1])
+    print("This is the value from history and should be the same \
+            as test accuracy", history["val_acc"])
+
+    print("This is training accuracy", history["acc"])
+
+    return [score[0], score[1]]
 
 
-
-model.compile(loss="categorical_crossentropy",
-              optimizer= RMSprop(),
-              metrics=["accuracy"])
-
-
-history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-
-score = model.evaluate(x_test, y_test, verbose = 1)
-print("Test loss: ", score[0])
-print("test accuracy: ", score[1])
+for num_layers in range(9, 10):
+    for nodes_per in range(9, 10):
+        appender = open("values.txt", "a")
+        values = model_create_and_run(num_layers, nodes_per)
+        appender.write(' '.join(values))
+        appender.close()
---
+++ plot_prototype.py
@@ -0,0 +1,7 @@
+# getting used to matplotlib
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection="3d")
