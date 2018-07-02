c8160f60552cf35a0ca0dac3c0e540d2863d397a
morabrandoi <morabrandoi@gmail.com>
2018-07-02 13:34:41 -0400

    cleaned bugs from line read method and added headers to text files

--- clean_data.py
+++ clean_data.py
@@ -2,7 +2,24 @@
 cleaned = open("cleaned_data.txt", "w")
 
 with open("layers0_5_nodes1_20.txt") as file:
-    line = file.readline()
-    numbers = line.split()
-    roundedLine = [round(float(num), 4) for num in numbers]
-    [cleaned.write(str(x)) for x in roundedLine]
+    header = file.readline()
+    cleaned.write(header)
+    while(1):
+
+
+        line = file.readline()
+
+        if (line == ""):
+            break
+
+        numbers = line.split()
+        roundedLine = [round(float(num), 4) for num in numbers]
+
+        roundedLine[0] += 1
+        ''' adding 1 here because by default the neural
+        network has 1 hidden layer and I started from zero
+        in the for loop
+        '''
+        [cleaned.write(str(x) + " ") for x in roundedLine]
+
+        cleaned.write("\n")
